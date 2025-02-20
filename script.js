60586d86734597fb6c8192bb845bc9459e101dbf
deoatharva <152968808+deoatharva@users.noreply.github.com>
2025-02-20 12:11:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,8 @@ let songs = [
     {songName: "Kaun Tujhe",filePath: "27.mp3", coverPath: "img.jpg"},
     {songName: "Ishq Wala Love",filePath: "28.mp3", coverPath: "img.jpg"},
     {songName: "tere liye",filePath: "29.mp3", coverPath: "img.jpg"},
-    {songName: "Atharva Shirsha",filePath: "30.mp3", coverPath: "img.jpg"}
+    {songName: "Atharva Shirsha",filePath: "30.mp3", coverPath: "img.jpg"},
+    {songName: "Aasman ko chukar",filePath: "31.mp3", coverPath: "img.jpg"}
 ]
 
 songItems.forEach((element, i)=>{
