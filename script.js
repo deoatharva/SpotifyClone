fd5788da17d86c01770f2e3b25d6e87f58193cf9
deoatharva <152968808+deoatharva@users.noreply.github.com>
2025-02-21 14:09:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ let songs = [
     {songName: "Man mera",filePath: "35.mp3", coverPath: "img.jpg"},
     {songName: "Tera yar hoon mai",filePath: "36.mp3", coverPath: "img.jpg"},
     {songName: "Hasi",filePath: "37.mp3", coverPath: "img.jpg"},
-    {songName: "Main Dhoondne Ko Zamaane Mein",filePath: "38.mp3", coverPath: "img.jpg"},   
+    {songName: "Dhondne Ko Zamane Mein",filePath: "38.mp3", coverPath: "img.jpg"},   
     {songName: "Bom Diggy Diggy ",filePath: "39.mp3", coverPath: "img.jpg"},
     {songName: "Bhool Bhulaiyaa 2 Title Track",filePath: "40.mp3", coverPath: "img.jpg"}
 ]
